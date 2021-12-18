6dad45f09243af2bd04ef8b4aa31c2278625f24a
Buzovskyi Vitalii <buzovskiy.v@gmail.com>
2021-12-19 00:27:29 +0200

    new commeit

--- image360upload/management/commands/unpack3d.py
+++ image360upload/management/commands/unpack3d.py
@@ -1,10 +1,11 @@
+import datetime
 import fnmatch
 import re
 
 from django.core.management.base import BaseCommand, CommandError
 from project.settings import MEDIA_ROOT
 import os
-import pathlib
+from pathlib import Path
 import zipfile
 import shutil
 from PIL import Image
@@ -16,39 +17,53 @@ class Command(BaseCommand):
 
     def __init__(self):
         super(Command, self).__init__()
-        pathlib.Path(self.root_path_3d).mkdir(parents=True, exist_ok=True)
+        Path(self.root_path_3d).mkdir(parents=True, exist_ok=True)
 
     def handle(self, *args, **options):
-        files = [f for f in os.listdir(pathlib.Path(self.root_path_3d))
+        files = [f for f in os.listdir(Path(self.root_path_3d))
                  if re.search(r'.*\.zip$', f, re.IGNORECASE)]
 
         if not len(files):
             return False
 
         for file in files:
-            dir_model_3d = os.path.splitext(pathlib.Path(self.root_path_3d) / file)[0]
-            # try:
-            #     shutil.rmtree(pathlib.Path(self.root_path_3d) / dir_model_3d)
-            # except OSError as e:
-            #     print("Error: %s : %s" % (pathlib.Path(self.root_path_3d) / dir_model_3d, e.strerror))
+            dir_model_name = os.path.splitext(os.path.basename(Path(self.root_path_3d) / file))[0]
+
+            self.remove_old_models(dir_model_name)
+
             # Create directories
-            datetime.date.today().strftime('%Y-%m-%d')
-            pathlib.Path(self.root_path_3d / dir_model_3d / 'images').mkdir(parents=True, exist_ok=True)
-            pathlib.Path(self.root_path_3d / dir_model_3d / 'imageslarge').mkdir(parents=True, exist_ok=True)
+            dir_model_path = self.root_path_3d / datetime.date.today().strftime('%Y-%m-%d') / dir_model_name
+            Path(dir_model_path / 'images').mkdir(parents=True, exist_ok=True)
+            Path(dir_model_path / 'imageslarge').mkdir(parents=True, exist_ok=True)
+
             # Extract images from archive
-            with zipfile.ZipFile(pathlib.Path(self.root_path_3d) / file, 'r') as zip_ref:
-                zip_ref.extractall(pathlib.Path(self.root_path_3d / dir_model_3d / 'imageslarge'))
+            with zipfile.ZipFile(Path(self.root_path_3d) / file, 'r') as zip_ref:
+                zip_ref.extractall(Path(dir_model_path / 'imageslarge'))
             # Resize images
-            images = [f for f in os.listdir(pathlib.Path(self.root_path_3d / dir_model_3d / 'imageslarge'))
+            images = [f for f in os.listdir(Path(dir_model_path / 'imageslarge'))
                       if re.search(r'.*\.(jpeg|jpg|gif|png|tiff|bmp)$', f, re.IGNORECASE)]
             if not len(images):
                 return False
 
             for image in images:
-                im = Image.open(pathlib.Path(self.root_path_3d / dir_model_3d / 'imageslarge' / image))
+                im = Image.open(Path(dir_model_path / 'imageslarge' / image))
                 newsize = (400, 288)
                 new_im = im.resize(newsize)
-                new_im.save(pathlib.Path(self.root_path_3d / dir_model_3d / 'images' / image))
+                new_im.save(Path(dir_model_path / 'images' / image))
+
+    def remove_old_models(self, dir_model_name):
+        matches = [Path(m) for m in list(Path(self.root_path_3d).glob(f"**/{dir_model_name}"))
+                   if Path(m).is_dir()]
+        if not len(matches):
+            return False
+
+        for match in matches:
+            try:
+                shutil.rmtree(match)
+            except OSError as e:
+                print("Error: %s : %s" % (match, e.strerror))
+
+
 
 
         # archives_path = pathlib.Path(self.root_path_3d).glob('*.zip')

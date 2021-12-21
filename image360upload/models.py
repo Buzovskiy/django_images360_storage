9e201dc9d3021bec7f679a2766bd081e7d1a16ae
Buzovskyi Vitalii <buzovskiy.v@gmail.com>
2021-12-21 09:44:42 +0200

    new commit

--- image360upload/models.py
+++ image360upload/models.py
@@ -1,4 +1,7 @@
 import os
+import pathlib
+import datetime
+import shutil
 
 from django.db import models
 from django.db.models.signals import post_delete, pre_save
@@ -8,13 +11,10 @@ from .validators import validate_file_extension
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 from django.conf import settings
-import pathlib
-import datetime
 
 
 def iframe_upload_to_function(instance, filename):
     """ this function has to return the location to upload the file """
-    # return pathlib.Path(settings.MEDIA_ROOT / '3d_models/models/%Y/%m/%d' / instance.vendor_code / filename)
     now = datetime.date.today()
     return f'3d_models/models/{now:%Y/%m/%d}/{instance.vendor_code}/{filename}'
 
@@ -105,6 +105,37 @@ def pre_save_archive(sender, instance, *args, **kwargs):
                 os.remove(old_file)
     except:
         pass
+
+
+@receiver(post_delete, sender=Image360)
+def post_delete_image360(sender, instance, *args, **kwargs):
+    """When we delete image360 instance, delete old image360 file and parent directory"""
+    try:
+        # Remember path to parent directory
+        parent = pathlib.Path(instance.iframe.path).parent
+        # Remove record
+        instance.iframe.delete(save=False)
+        print(parent)
+        # Remove parent
+        shutil.rmtree(parent)
+    except:
+        pass
+
+
+# @receiver(pre_save, sender=Model3dArchive)
+# def pre_save_archive(sender, instance, *args, **kwargs):
+#     """When update archive, delete old archive file.Instance old archive file will delete from os """
+#     try:
+#         old_file = instance.__class__.objects.get(pk=instance.id).archive.path
+#         try:
+#             new_file = instance.archive.path
+#         except:
+#             new_file = None
+#         if new_file != old_file:
+#             if os.path.exists(old_file):
+#                 os.remove(old_file)
+#     except:
+#         pass
 
 
 
